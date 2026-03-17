0230544a24d86e794e02d55dd7740edeefe9eb21
disharaghav <disharaghav0@gmail.com>
2026-03-17 23:25:48 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "https://resume-ai-pe89.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # 🔥 IMPORTANT (not "*")
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from mangum import Mangum
 
 from api.database import *
